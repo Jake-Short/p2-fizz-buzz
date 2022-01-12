7b4cf64ce1de4667572c32ca596c940aebeb1db8
Jake Short <46853220+Jake-Short@users.noreply.github.com>
2022-01-11 23:45:56 -0800

    Initial Commit

---
+++ p2-fizz-buzz/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  p2-fizz-buzz
+//
+//  Created by Jake Short on 1/11/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
